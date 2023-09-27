04ef858d87d951ec0644cc9e36f786f2f73d9f44
Nurfaiz <nurfaizfy19@gmail.com>
2023-09-27 20:40:47 +0700

    tools: Import AnyKernel3 for ruby from https://github.com/Gonon-Kernel/AnyKernel3

---
+++ tools/AnyKernel3/anykernel.sh
@@ -0,0 +1,40 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=ExampleKernel by osm0sis @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=ruby
+supported.versions=13
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+# boot shell variables
+block=auto;
+is_slot_device=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+split_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+flash_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
